b6711bfd5eead8ea9066601c3e4ae9fea18a8a77
SanektAnnadate <sanketannadatejain@gmail.com>
2024-12-18 00:11:39 +0530

    Feing and swagger implemented

--- src/main/java/com/om/order/service/OrderService.java
+++ src/main/java/com/om/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.om.order.service;
 
+import com.om.order.client.InventoryClient;
 import com.om.order.dto.OrderRequest;
 import com.om.order.entity.Order;
 import com.om.order.repository.OrderRepository;
@@ -9,20 +10,27 @@ import org.springframework.stereotype.Service;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, InventoryClient inventoryClient) {
         this.orderRepository = orderRepository;
+        this.inventoryClient = inventoryClient;
     }
 
     public OrderRequest placeOrder(OrderRequest orderRequest) {
-        Order order = orderRepository.save(new Order(orderRequest.orderNumber(),
-                orderRequest.skuCode(),
-                orderRequest.price(),
-                orderRequest.quantity()));
-        return new OrderRequest(order.getId(),
-                order.getOrderNumber(),
-                order.getSkuCode(),
-                order.getPrice(),
-                order.getQuantity());
+        if(inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity()))
+        {
+            Order order = orderRepository.save(new Order(orderRequest.orderNumber(),
+                    orderRequest.skuCode(),
+                    orderRequest.price(),
+                    orderRequest.quantity()));
+            return new OrderRequest(order.getId(),
+                    order.getOrderNumber(),
+                    order.getSkuCode(),
+                    order.getPrice(),
+                    order.getQuantity());
+        }else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
     }
 }
